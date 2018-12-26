088f5641dc644242fc2e71c6158ce3a4dbaa0a05
LucisAeternae <iampeacemans@gmail.com>
2018-12-26 14:06:49 +0300

    updated models \w population-script local only

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 class Game(models.Model):
     name = models.CharField(max_length=128, unique=True)
     purchases = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField()
     def __str__(self):
         return self.name
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Game, self).save(*args, **kwargs)
 
 class Category(models.Model):
     title = models.CharField(max_length=128)
@@ -15,6 +19,11 @@ class Category(models.Model):
     slug = models.SlugField()
     def __str__(self):
         return self.title
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+    class Meta():
+        verbose_name_plural = "Categories"
 
 class Subcategory(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
@@ -22,10 +31,19 @@ class Subcategory(models.Model):
     title = models.CharField(max_length=128)
     def __str__(self):
         return self.title
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Subcategory, self).save(*args, **kwargs)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
-
+    username = models.CharField(unique=True, max_length=191)
+    slug = models.SlugField()
+    def __str__(self):
+        return self.username
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super(Profile, self).save(*args, **kwargs)
 
 class Offer(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
@@ -37,3 +55,5 @@ class Offer(models.Model):
         default=0,
         default_currency='USD',
         max_digits=11,)
+    def __str__(self):
+        return self.title
